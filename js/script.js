fc4d37b12b0110719e55e5e5b5ce148d4c13645f
longpatrick <long.patrick1997@gmail.com>
2021-03-04 19:05:48 -0600

    near-final: fixed bugs; added checkbox functionality

--- js/script.js
+++ js/script.js
@@ -14,6 +14,12 @@ const $screenshot1 = $('#screenshot1');
 const $screenshot2 = $('#screenshot2');
 const $copyright = $('#copyright');
 const $searchBar = $('#search-bar');
+const $publisherCheckbox = $('#checkbox1');
+const $metacriticCheckbox = $('#checkbox2');
+const $ratingCheckbox = $('#checkbox3');
+const $releaseCheckbox = $('#checkbox4');
+const $genresCheckbox = $('#checkbox5');
+const $websiteCheckbox = $('#checkbox6');
 
 const BASE_URL = 'https://api.rawg.io/api/games';
 const API_KEY = '2382cc10c0784e578a1005c8d9b3c447';
@@ -21,6 +27,14 @@ const UPCOMING_URL = 'https://api.rawg.io/api/games?dates=2021-03-01,2021-12-31&
 
 // event listeners
 $('form').on('submit', handleSubmit);
+
+// default setting for checkboxes
+$($publisherCheckbox).prop("checked", true);
+$($metacriticCheckbox).prop("checked", true);
+$($ratingCheckbox).prop("checked", true);
+$($releaseCheckbox).prop("checked", true);
+$($genresCheckbox).prop("checked", true);
+$($websiteCheckbox).prop("checked", true);
 
 // functions
 function handleSubmit(event) {
@@ -42,51 +56,65 @@ const renderUser = () => {
         $name.text(gameData[0].name);
         $('#d').html('Description:');
         $description.text(gameData[0].description_raw);
-        if (gameData[0].publishers[0] !== null && gameData[0].publishers[0] !== null && !!gameData[0].publishers[0].name) {
-            $('#p').html('Publisher:');
-            $publisher.text(gameData[0].publishers[0].name);
+        if ($publisherCheckbox[0].checked === true) {
+            if (gameData[0].publishers[0] !== null && gameData[0].publishers[0] !== null && !!gameData[0].publishers[0].name) {
+                $('#p').html('Publisher:');
+                $publisher.text(gameData[0].publishers[0].name);
+            }
         } else {
             $publisher.hide();
             $('#p').hide();
         }
-        if (!!gameData[0].metacritic) {
-            $('#m').html('Metacritic Rating:');
-            $metacritic.text(`${gameData[0].metacritic}/100`);
+        if ($metacriticCheckbox[0].checked === true) {
+            if (!!gameData[0].metacritic) {
+                $('#m').html('Metacritic Rating:');
+                $metacritic.text(`${gameData[0].metacritic}/100`);
+            }
         } else {
             $metacritic.hide();
             $('#m').hide();
         }
-        if (gameData[0].esrb_rating !== null && !!gameData[0].esrb_rating.name) {
-            $('#r').html('ESRB Rating:');
-            $rating.text(gameData[0].esrb_rating.name);
+        if ($ratingCheckbox[0].checked === true) {
+            if (gameData[0].esrb_rating !== null && !!gameData[0].esrb_rating.name) {
+                $('#r').html('ESRB Rating:');
+                $rating.text(gameData[0].esrb_rating.name);
+            }
         } else {
             $rating.hide();
             $('#r').hide();
         }
-        if (!!gameData[0].released) {
-            $('#re').html('Release Date:');
-            $releaseDate.text(gameData[0].released);
+        if ($releaseCheckbox[0].checked === true) {
+            if (!!gameData[0].released) {
+                $('#re').html('Release Date:');
+                $releaseDate.text(gameData[0].released);
+            }
         } else {
             $releaseDate.hide();
             $('#re').hide();
         }
-        if (gameData[0].genres.length === 1) {
-            $('#g').html('Genre(s):');
-            $genres.text(`${gameData[0].genres[0].name}`);
+        if ($genresCheckbox[0].checked === true) {
+            if (gameData[0].genres.length === 1) {
+                $('#g').html('Genre(s):');
+                $genres.text(`${gameData[0].genres[0].name}`);
+            } else {
+                $genres.text(`${gameData[0].genres[0].name}, ${gameData[0].genres[1].name}`);
+            }
         } else {
-            $genres.text(`${gameData[0].genres[0].name}, ${gameData[0].genres[1].name}`);
+            $('#g').text('');
+            $genres.text('');
         }
-        if (!!gameData[0].website) {
-            $('#w').text('Website:');
-            $website.text(gameData[0].website);
-            $website.attr('href', gameData[0].website);
+        if ($websiteCheckbox[0].checked === true) {
+            if (!!gameData[0].website) {
+                $('#w').text('Website:');
+                $website.text(gameData[0].website);
+                $website.attr('href', gameData[0].website);
+            }
         } else {
             $('#w').text('');
             $website.text('');
         }
         if (!!gameData[0].background_image) {
             $screenshot1.attr('src', gameData[0].background_image);
-            $screenshot1.width('90%');
         }
         if (!!gameData[0].background_image_additional) {
             $screenshot2.attr('src', gameData[0].background_image_additional);
@@ -106,18 +134,19 @@ $.ajax(`${UPCOMING_URL}?key=${API_KEY}`).then(data => {
 
 const renderUpcoming = () => {
 // first upcoming game details
-    $('.upcomingCards').append(`<h2 class="name">${gameData[0].results[0].name}</h2><img src="${gameData[0].results[0].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[0].released}</p><p class="g">Genre: ${gameData[0].results[0].genres[0].name}</p><hr>`);
+    $('.upcomingCards').append(`<div class="upcomingCard1"><h2 class="name">${gameData[0].results[0].name}</h2><img src="${gameData[0].results[0].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[0].released}</p><p class="g">Genre: ${gameData[0].results[0].genres[0].name}</p></div><hr>`);
 
 // second upcoming game details
-    $('.upcomingCards').append(`<h2 class="name">${gameData[0].results[1].name}</h2><img src="${gameData[0].results[1].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[1].released}</p><p class="g">Genre: ${gameData[0].results[1].genres[0].name}</p><hr>`);
+    $('.upcomingCards').append(`<div class="upcomingCard2"><h2 class="name">${gameData[0].results[1].name}</h2><img src="${gameData[0].results[1].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[1].released}</p><p class="g">Genre: ${gameData[0].results[1].genres[0].name}</p></div><hr>`);
 
 // third upcoming game details
-    $('.upcomingCards').append(`<h2 class="name">${gameData[0].results[2].name}</h2><img src="${gameData[0].results[2].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[2].released}</p><p class="g">Genre: ${gameData[0].results[2].genres[0].name}</p><hr>`);
+    $('.upcomingCards').append(`<div class="upcomingCard3"><h2 class="name">${gameData[0].results[2].name}</h2><img src="${gameData[0].results[2].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[2].released}</p><p class="g">Genre: ${gameData[0].results[2].genres[0].name}</p></div><hr>`);
 
 // fourth upcoming game details
-    $('.upcomingCards').append(`<h2 class="name">${gameData[0].results[3].name}</h2><img src="${gameData[0].results[3].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[3].released}</p><p class="g">Genre: ${gameData[0].results[3].genres[0].name}</p><hr>`);
+    $('.upcomingCards').append(`<div class="upcomingCard4"><h2 class="name">${gameData[0].results[3].name}</h2><img src="${gameData[0].results[3].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[3].released}</p><p class="g">Genre: ${gameData[0].results[3].genres[0].name}</p></div><hr>`);
 
 // fifth upcoming game details
-    $('.upcomingCards').append(`<h2 class="name">${gameData[0].results[4].name}</h2><img src="${gameData[0].results[4].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[4].released}</p><p class="g">Genre: ${gameData[0].results[4].genres[0].name}</p>`);
+    $('.upcomingCards').append(`<div class="upcomingCard5"><h2 class="name">${gameData[0].results[4].name}</h2><img src="${gameData[0].results[4].background_image}" class="upcomingImage"><p class="re">Release Date: ${gameData[0].results[4].released}</p><p class="g">Genre: ${gameData[0].results[4].genres[0].name}</p></div>`);
 };
+
 
